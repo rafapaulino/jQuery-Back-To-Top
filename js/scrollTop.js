a62cfde1f7c2683204df093574afceaae0737cf9
Rafael Brigagao Paulino <rafael.paulino@FIESP.COM.BR>
2018-08-14 10:40:52 -0300

    css and js

---
+++ js/scrollTop.js
@@ -0,0 +1,31 @@
+(function ( $ ) {
+ 
+    $.fn.scrollTop = function() {
+        var $scrollTop = this;
+            
+        $(window).scroll(function() {
+            // declare variable
+            var topPos = $(this).scrollTop();
+        
+            // if user scrolls down - show scroll to top button
+            if (topPos > 100) {
+                $scrollTop.css("opacity", "1");
+            } else {
+                $scrollTop.css("opacity", "0");
+            }
+        
+        }); // scroll END
+
+        //Click event to scroll to top
+        $scrollTop.on('click', function() {
+            $('html, body').animate({
+            scrollTop: 0
+            }, 800);
+            return false;
+
+        }); // click() scroll top EMD
+
+        return this;
+    };
+ 
+}( jQuery ));
